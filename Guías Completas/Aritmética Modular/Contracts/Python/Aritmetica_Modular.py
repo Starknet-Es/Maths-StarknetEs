802b63c38174fd3909a7c06d2573a0cbdd65d7f6
Nadai <112663528+Nadai2010@users.noreply.github.com>
2023-03-02 07:34:20 +0000

    🧮 Código para Python

--- Guías Completas/Aritmética Modular/Contracts/Python/Aritmetica_Modular.py
+++ Guías Completas/Aritmética Modular/Contracts/Python/Aritmetica_Modular.py
@@ -1,1 +1,23 @@
+def method_1(a,b):
 
+    for i in range(1,509):
+        result = (a ** i) % b
+        print("Result", i,":", result)
+
+print("Method 1:")
+print("")
+method_1(499,509)
+
+print("")
+
+def method_2(a,b):
+
+    for i in range(1,509):
+        result = (a ** i) % b
+        if result == 466:
+            print("Result is:",i)
+
+
+print("Method 2:")
+print("")
+method_2(499,509)
